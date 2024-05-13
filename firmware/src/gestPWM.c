a91aee7ec590c98c99aac8c5fd328d6abd1cb232
CyrilFeliciano <113518224+CyrilFeliciano@users.noreply.github.com>
2024-05-13 18:06:44 +0200

    Update gestPWM.c

--- firmware/src/gestPWM.c
+++ firmware/src/gestPWM.c
@@ -19,19 +19,19 @@
 /* Fonction :
     void GPWM_Initialize(S_pwmSettings *pData)
 
-  Résumé :
-    Initialise la structure de paramètres PWM et configure le matériel associé.
+  RÃ©sumÃ© :
+    Initialise la structure de paramÃ¨tres PWM et configure le matÃ©riel associÃ©.
 
   Description :
-    Cette fonction initialise la structure de données S_pwmSettings pointée par
-    pData en mettant à zéro ses membres relatifs à l'angle, à la vitesse, et à
-    leurs valeurs absolues. Ensuite, elle configure l'état du pont en H à l'aide
-    de la fonction BSP_EnableHbrige(). Enfin, elle démarre les timers et les
-    sorties de comparaison (OC - Output Compare) nécessaires pour la génération
-    de signaux PWM en appelant les fonctions appropriées de la bibliothèque DRV.
+    Cette fonction initialise la structure de donnÃ©es S_pwmSettings pointÃ©e par
+    pData en mettant Ã  zÃ©ro ses membres relatifs Ã  l'angle, Ã  la vitesse, et Ã 
+    leurs valeurs absolues. Ensuite, elle configure l'Ã©tat du pont en H Ã  l'aide
+    de la fonction BSP_EnableHbrige(). Enfin, elle dÃ©marre les timers et les
+    sorties de comparaison (OC - Output Compare) nÃ©cessaires pour la gÃ©nÃ©ration
+    de signaux PWM en appelant les fonctions appropriÃ©es de la bibliothÃ¨que DRV.
 
-  Paramètres :
-    - pData : Un pointeur vers la structure de paramètres PWM (S_pwmSettings).
+  ParamÃ¨tres :
+    - pData : Un pointeur vers la structure de paramÃ¨tres PWM (S_pwmSettings).
 
    
 */
@@ -39,13 +39,13 @@
 
 void GPWM_Initialize(S_pwmSettings *pData)
 {
-    // Initialise les données de la structure pData
+    // Initialise les donnÃ©es de la structure pData
     pData->AngleSetting = 0;
     pData->SpeedSetting = 0;
     pData->absAngle = 0;
     pData->absSpeed = 0;
     
-    // Initialise l'état du pont en H
+    // Initialise l'Ã©tat du pont en H
     BSP_EnableHbrige();
     
     // Lance les timers et les sorties de comparaison (OC - Output Compare)
@@ -61,20 +61,20 @@ void GPWM_Initialize(S_pwmSettings *pData)
 /* Fonction :
     void GPWM_GetSettings(S_pwmSettings *pData)
 
-  Résumé :
-    Obtient les valeurs de vitesse et d'angle à partir du convertisseur AD.
+  RÃ©sumÃ© :
+    Obtient les valeurs de vitesse et d'angle Ã  partir du convertisseur AD.
 
   Description :
-    Cette fonction récupère les valeurs de vitesse et d'angle à partir du
-    convertisseur analogique-numérique (AD). Elle lit les valeurs du canal 0
+    Cette fonction rÃ©cupÃ¨re les valeurs de vitesse et d'angle Ã  partir du
+    convertisseur analogique-numÃ©rique (AD). Elle lit les valeurs du canal 0
     (vitesse) et du canal 1 (angle) du convertisseur AD, effectue une moyenne
-    sur un certain nombre d'échantillons (défini par TAILLE_MOYENNE_ADC) pour
-    réduire les variations du signal, puis effectue une conversion en unités
-    appropriées.
+    sur un certain nombre d'Ã©chantillons (dÃ©fini par TAILLE_MOYENNE_ADC) pour
+    rÃ©duire les variations du signal, puis effectue une conversion en unitÃ©s
+    appropriÃ©es.
 
-  Paramètres :
-    - pData : Un pointeur vers la structure de paramètres PWM (S_pwmSettings)
-              où seront stockées les valeurs de vitesse et d'angle.
+  ParamÃ¨tres :
+    - pData : Un pointeur vers la structure de paramÃ¨tres PWM (S_pwmSettings)
+              oÃ¹ seront stockÃ©es les valeurs de vitesse et d'angle.
 
 */
 // *****************************************************************************
@@ -92,7 +92,7 @@ void GPWM_GetSettings(S_pwmSettings *pData)
     int32_t valeur_variant_ADC1, valeur_variant_ADC2;
     APP_DATA appData;
 
-    // Lire les valeurs du convertisseur analogique-numérique
+    // Lire les valeurs du convertisseur analogique-numÃ©rique
     appData.AdcRes = BSP_ReadADCAlt();
     valeur_ADC1[i] = appData.AdcRes.Chan0;
     valeur_ADC2[i] = appData.AdcRes.Chan1;
@@ -102,7 +102,7 @@ void GPWM_GetSettings(S_pwmSettings *pData)
         i = 0;
     }
 
-    // Calculer la moyenne des échantillons pour lisser le signal
+    // Calculer la moyenne des Ã©chantillons pour lisser le signal
     for (n = 0; n < TAILLE_MOYENNE_ADC; n++)
     {
         somme1 += valeur_ADC1[n];
@@ -111,12 +111,12 @@ void GPWM_GetSettings(S_pwmSettings *pData)
     moyen_ADC1 = somme1 / TAILLE_MOYENNE_ADC;
     moyen_ADC2 = somme2 / TAILLE_MOYENNE_ADC;
 
-    // Conversion des valeurs ADC en unités appropriées
+    // Conversion des valeurs ADC en unitÃ©s appropriÃ©es
     valeur_variant_ADC1 = ((198 * moyen_ADC1) / 1023) + 0.5;
     valeur_variant_ADC1 = valeur_variant_ADC1 - 99;
     valeur_variant_ADC2 = ((180 * moyen_ADC2) / 1023) + 0.5;
 
-    // Stockage des valeurs converties dans la structure de paramètres
+    // Stockage des valeurs converties dans la structure de paramÃ¨tres
     pData->absAngle = valeur_variant_ADC2;
     valeur_variant_ADC2 = (valeur_variant_ADC2 - 90);
     pData->AngleSetting = valeur_variant_ADC2;
@@ -130,18 +130,18 @@ void GPWM_GetSettings(S_pwmSettings *pData)
 /* Fonction :
     void GPWM_DispSettings(S_pwmSettings *pData)
 
-  Résumé :
-    Affiche les informations de la structure de paramètres sur l'afficheur LCD.
+  RÃ©sumÃ© :
+    Affiche les informations de la structure de paramÃ¨tres sur l'afficheur LCD.
 
   Description :
-    Cette fonction prend en entrée une structure de paramètres PWM (pData) et
+    Cette fonction prend en entrÃ©e une structure de paramÃ¨tres PWM (pData) et
     affiche les informations de vitesse (SpeedSetting, absSpeed) et d'angle
-    (AngleSetting) sur un afficheur LCD. Les valeurs sont formatées pour
-    afficher le signe et la valeur absolue des paramètres.
-    Elle gère egalement l'affichage du mode local et remote.
+    (AngleSetting) sur un afficheur LCD. Les valeurs sont formatÃ©es pour
+    afficher le signe et la valeur absolue des paramÃ¨tres.
+    Elle gÃ¨re egalement l'affichage du mode local et remote.
 
-  Paramètres :
-    - pData : Un pointeur vers la structure de paramètres PWM (S_pwmSettings)
+  ParamÃ¨tres :
+    - pData : Un pointeur vers la structure de paramÃ¨tres PWM (S_pwmSettings)
               contenant les informations de vitesse et d'angle.
  *  - Remote : Variable qui me permet de savoir si je suis en mode local ou remote.
 
@@ -149,42 +149,44 @@ void GPWM_GetSettings(S_pwmSettings *pData)
 // *****************************************************************************
 void GPWM_DispSettings(S_pwmSettings *pData, int Remote)
 {
-    static uint8_t compteurClearLine = 0;
+    static uint8_t compteurAffichageLcd = 0;
+    static uint8_t compteurClearLine;
     
-        // Si les lignes du LCD n'ont pas été effacées auparavant (compteurClearLine est à 0), efface les lignes et affiche les titres.
-    if(compteurClearLine == 0)
+        // Si les lignes du LCD n'ont pas Ã©tÃ© effacÃ©es auparavant (compteurClearLine est Ã  0), efface les lignes et affiche les titres.
+    if(compteurAffichageLcd == 0)
     {
-        lcd_ClearLine(1);
-        lcd_ClearLine(2);
-        lcd_ClearLine(3);
-        lcd_ClearLine(4);
-        lcd_gotoxy(1,2); // Déplacement du curseur à la position (1,2) pour afficher "SpeedSetting".
+        for(compteurClearLine = 1; compteurClearLine < cinq; compteurClearLine++)
+        {
+             lcd_ClearLine(compteurClearLine);
+        }
+        
+        lcd_gotoxy(1,2); // DÃ©placement du curseur Ã  la position (1,2) pour afficher "SpeedSetting".
         printf_lcd("SpeedSetting");
-        lcd_gotoxy(1,3); // Déplacement du curseur à la position (1,3) pour afficher "absSpeed".
+        lcd_gotoxy(1,3); // DÃ©placement du curseur Ã  la position (1,3) pour afficher "absSpeed".
         printf_lcd("absSpeed");
-        lcd_gotoxy(1,4); // Déplacement du curseur à la position (1,4) pour afficher "Angle".
+        lcd_gotoxy(1,4); // DÃ©placement du curseur Ã  la position (1,4) pour afficher "Angle".
         printf_lcd("Angle");
-        compteurClearLine++; // Incrémente le compteur pour indiquer que les lignes ont été effacées.
+        compteurAffichageLcd++; // IncrÃ©mente le compteur pour indiquer que les lignes ont Ã©tÃ© effacÃ©es.
     }
  
-    // Affiche "Local Settings" ou "** Remote Settings" sur la première ligne du LCD en fonction de la valeur de Remote.
+    // Affiche "Local Settings" ou "** Remote Settings" sur la premiÃ¨re ligne du LCD en fonction de la valeur de Remote.
     if(Remote == 0)
     {   
-        lcd_gotoxy(1,1); // Déplacement du curseur à la position (1,1) pour afficher le statut des réglages.
+        lcd_gotoxy(1,1); // DÃ©placement du curseur Ã  la position (1,1) pour afficher le statut des rÃ©glages.
         printf_lcd("Local Settings    ");
     }
     else
     {
-        lcd_gotoxy(1,1); // Déplacement du curseur à la position (1,1) pour afficher le statut des réglages.
+        lcd_gotoxy(1,1); // DÃ©placement du curseur Ã  la position (1,1) pour afficher le statut des rÃ©glages.
         printf_lcd("** Remote Settings");
     }
     
         // Affiche les valeurs de vitesse, de vitesse absolue et d'angle sur le LCD.
-        lcd_gotoxy(14,2); // Déplacement du curseur à la position (14,2) pour afficher la vitesse de réglage.
+        lcd_gotoxy(14,2); // DÃ©placement du curseur Ã  la position (14,2) pour afficher la vitesse de rÃ©glage.
         printf_lcd("%3d ", pData->SpeedSetting);
-        lcd_gotoxy(15,3); // Déplacement du curseur à la position (15,3) pour afficher la vitesse absolue.
+        lcd_gotoxy(15,3); // DÃ©placement du curseur Ã  la position (15,3) pour afficher la vitesse absolue.
         printf_lcd("%2d ", pData->absSpeed);
-        lcd_gotoxy(14,4); // Déplacement du curseur à la position (14,4) pour afficher l'angle de réglage.
+        lcd_gotoxy(14,4); // DÃ©placement du curseur Ã  la position (14,4) pour afficher l'angle de rÃ©glage.
         printf_lcd("%3d ", pData->AngleSetting);
 }
 
@@ -199,21 +201,21 @@ void GPWM_DispSettings(S_pwmSettings *pData, int Remote)
 /* Fonction :
     void GPWM_ExecPWM(S_pwmSettings *pData)
 
-  Résumé :
-    Exécute la modulation de largeur d'impulsion (PWM) et gère le moteur en
-    fonction des informations contenues dans la structure de paramètres.
+  RÃ©sumÃ© :
+    ExÃ©cute la modulation de largeur d'impulsion (PWM) et gÃ¨re le moteur en
+    fonction des informations contenues dans la structure de paramÃ¨tres.
 
   Description :
-    Cette fonction prend en entrée une structure de paramètres PWM (pData)
+    Cette fonction prend en entrÃ©e une structure de paramÃ¨tres PWM (pData)
     contenant les informations sur la vitesse (SpeedSetting) et l'angle
-    (AngleSetting). En fonction de la vitesse, elle contrôle l'état du pont en H
-    pour définir la direction du moteur. Ensuite, elle calcule la largeur
+    (AngleSetting). En fonction de la vitesse, elle contrÃ´le l'Ã©tat du pont en H
+    pour dÃ©finir la direction du moteur. Ensuite, elle calcule la largeur
     d'impulsion (Pulse Width) en utilisant les informations de vitesse et d'angle
-    pour les sorties de comparaison OC2 et OC3. Elle utilise la bibliothèque PLIB
-    pour effectuer ces opérations.
+    pour les sorties de comparaison OC2 et OC3. Elle utilise la bibliothÃ¨que PLIB
+    pour effectuer ces opÃ©rations.
 
-  Paramètres :
-    - pData : Un pointeur vers la structure de paramètres PWM (S_pwmSettings)
+  ParamÃ¨tres :
+    - pData : Un pointeur vers la structure de paramÃ¨tres PWM (S_pwmSettings)
               contenant les informations de vitesse et d'angle.
 
 */
@@ -223,7 +225,7 @@ void GPWM_ExecPWM(S_pwmSettings *pData)
     static uint16_t PulseWidthOC2;
     static uint16_t PulseWidthOC3;
 
-    // Contrôle de l'état du pont en H en fonction de la vitesse
+    // ContrÃ´le de l'Ã©tat du pont en H en fonction de la vitesse
     if (pData->SpeedSetting < 0)
     {
         PLIB_PORTS_PinSet(PORTS_ID_0, AIN1_HBRIDGE_PORT, AIN1_HBRIDGE_BIT);
@@ -255,19 +257,19 @@ void GPWM_ExecPWM(S_pwmSettings *pData)
 /* Fonction :
     void GPWM_ExecPWMSoft(S_pwmSettings *pData)
 
-  Résumé :
-    Exécute la modulation de largeur d'impulsion (PWM) logiciel.
+  RÃ©sumÃ© :
+    ExÃ©cute la modulation de largeur d'impulsion (PWM) logiciel.
 
   Description :
-    Cette fonction prend en entrée une structure de paramètres PWM (pData)
+    Cette fonction prend en entrÃ©e une structure de paramÃ¨tres PWM (pData)
     contenant les informations sur la vitesse (SpeedSetting). Elle utilise une
-    méthode logicielle pour simuler une modulation de largeur d'impulsion. La LED
-    BSP_LED_2 est éteinte si la valeur absolue de la vitesse est supérieure au
-    compteur compteurRpc, sinon elle est allumée. Le compteurRpc est incrémenté
-    à chaque appel de la fonction et remis à zéro s'il dépasse la valeur 99.
+    mÃ©thode logicielle pour simuler une modulation de largeur d'impulsion. La LED
+    BSP_LED_2 est Ã©teinte si la valeur absolue de la vitesse est supÃ©rieure au
+    compteur compteurRpc, sinon elle est allumÃ©e. Le compteurRpc est incrÃ©mentÃ©
+    Ã  chaque appel de la fonction et remis Ã  zÃ©ro s'il dÃ©passe la valeur 99.
 
-  Paramètres :
-    - pData : Un pointeur vers la structure de paramètres PWM (S_pwmSettings)
+  ParamÃ¨tres :
+    - pData : Un pointeur vers la structure de paramÃ¨tres PWM (S_pwmSettings)
               contenant les informations de vitesse.
 
 */
@@ -276,7 +278,7 @@ void GPWM_ExecPWMSoft(S_pwmSettings *pData)
 {
     static uint8_t compteurRpc = 0;
 
-    // Si la valeur absolue de la vitesse est supérieure au compteurRpc, éteindre la LED
+    // Si la valeur absolue de la vitesse est supÃ©rieure au compteurRpc, Ã©teindre la LED
     if (pData->absSpeed > compteurRpc)
     {
         BSP_LEDOff(BSP_LED_2);
@@ -287,10 +289,10 @@ void GPWM_ExecPWMSoft(S_pwmSettings *pData)
         BSP_LEDOn(BSP_LED_2);
     }
 
-    // Incrémente le compteurRpc
+    // IncrÃ©mente le compteurRpc
     compteurRpc++;
 
-    // Remet à zéro le compteurRpc s'il dépasse la valeur 99
+    // Remet Ã  zÃ©ro le compteurRpc s'il dÃ©passe la valeur 99
     if (compteurRpc > 99)
     {
         compteurRpc = 0;
